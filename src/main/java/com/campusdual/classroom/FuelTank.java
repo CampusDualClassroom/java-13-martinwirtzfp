823ce03b1b34d1fbb64a2e3e4085fe181e021693
Martin Sanchez Novo <martinwirtzfp@gmail.com>
2025-05-23 10:06:04 +0200

    Ejercicio 13 realizado y testeado

--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -2,10 +2,19 @@ package com.campusdual.classroom;
 
 public class FuelTank {
 
-  public int actualFuel = 10;
+  private int actualFuel = 10;
 
   public void showDetails() {
     System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
+  }
+
+  public void setActualFuel(int fuel){
+    if (fuel < 0){
+      System.out.println("No se admiten valores negativos");
+    } else {
+      this.actualFuel = fuel;
+    }
+
   }
 
   public int getActualFuel() {
